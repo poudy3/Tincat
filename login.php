4312c2e122b39037e3a2fccf7e33e21e26f0bc72
thomas mouchelet <mouchelet.thomas@gmail.com>
2019-12-09 16:30:02 +0100

    css register

---
+++ login.php
@@ -0,0 +1,1 @@
+<?php require "head.php"; ?>
--- register.php
+++ register.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+<?php require "head.php"; ?>
+
 <body>
-    <form action="">
-        <input type="text" placeholder="pseudo">
-        <input type="password" placeholder="password">
-    </form>
+    <div class="form-container">
+        <h1>TINCAT</h1>
+        <form action="">
+            <input type="text" placeholder="pseudo">
+            <input type="password" placeholder="password">
+            <input type="submit" value="register">
+        </form>
+    </div>
 </body>
 </html>
